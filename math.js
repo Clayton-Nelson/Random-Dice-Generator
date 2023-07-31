35bb58c3655c7aeb3fb5525963fa2e753957133a
ClaytonN <Clayton.nelson03@gmail.com>
2023-07-31 21:57:36 +0930

    This is a commit for the Javascript for the random dice rolling

---
+++ math.js
@@ -0,0 +1,30 @@
+
+function numbergend4 (){
+        var random_num= Math.floor(Math.random()*(4)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
+
+function numbergend6 (){
+        var random_num= Math.floor(Math.random()*(6)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
+
+function numbergend8 (){
+        var random_num= Math.floor(Math.random()*(8)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
+
+function numbergend10 (){
+        var random_num= Math.floor(Math.random()*(10)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
+
+function numbergend12 (){
+        var random_num= Math.floor(Math.random()*(12)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
+
+function numbergend20 (){
+        var random_num= Math.floor(Math.random()*(20)+1);
+        document.getElementById("output").innerHTML = random_num;
+    }
